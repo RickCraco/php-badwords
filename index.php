289a405f1f57c12237cf335e07ad3230a4e0f594
RickCraco <riccardocracolici43@gmail.com>
2023-11-27 16:00:50 +0100

    aggiunto form con vari campi

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>Form PHP</h1>
+    <form action="censura.php" method="GET">
+        <label for="paragraph">Inserisci un paragrafo: </label>
+        <textarea name="paragraph" id="paragraph" required></textarea>
+
+        <label for="censura">Parola da censurare</label>
+        <input type="text" name="censura" id="censura" required>
+
+        <input type="submit" value="invia">
+    </form>
 </body>
 </html>
